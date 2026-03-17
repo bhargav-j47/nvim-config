39f472b09ba1ce90ee0045e2e56d7a9408b29faf
bhargav-j47 <bhargavjoshi2005@gmail.com>
2026-03-17 11:20:09 +0530

    add custom command to enable 8 spaces tabs required for linux dev
    Signed-off-by: bhargav-j47 <bhargavjoshi2005@gmail.com>

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,14 +1,28 @@
-vim.cmd("set number")
-vim.cmd("set mouse=a")
-vim.cmd("syntax enable")
-vim.cmd("set showcmd")
-vim.cmd("set encoding=utf-8")
-vim.cmd("set showmatch")
-vim.cmd("set relativenumber")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=0")
-vim.cmd("set softtabstop=0")
-vim.cmd("set autoindent")
-vim.cmd("set smarttab")
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
 
+-- General / UI
+vim.opt.encoding = "utf-8"
+vim.opt.mouse = "a"
+vim.opt.showcmd = true
+vim.opt.showmatch = true
+
+-- Tabs & Indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+vim.opt.softtabstop = 0
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+
+-- Syntax highlighting is a command, not a standard option
+vim.cmd.syntax("enable")
+
+vim.api.nvim_create_user_command('Linuxtabs', function()
+    vim.opt.tabstop = 8
+    vim.opt.softtabstop = 8
+    vim.opt.shiftwidth = 8
+    vim.opt.expandtab = false
+    print("8-space hard tabs enabled.")
+end, { desc = 'Temporarily set 8-space hard tabs' })
