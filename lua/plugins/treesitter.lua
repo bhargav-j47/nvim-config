5212567d0bfabaef7dd96975525e5d40dcfe5e14
bhargav-j47 <bhargavjoshi2005@gmail.com>
2026-05-11 23:07:22 +0530

    config: cleanup: formatting changes
    Signed-off-by: bhargav-j47 <bhargavjoshi2005@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,10 +4,10 @@ return {
     config = function()
         local configs = require("nvim-treesitter")
         configs.setup({
-            ensure_installed = {"lua", "c", "javascript" , "python" ,"cpp",
+            ensure_installed = {"lua", "c", "python" ,"cpp",
                                 "go", "gomod", "gowork", "gosum", -- Go ecosystem
                                 "rust", "bash", "typescript", "tsx",
-                                "html", "css",                    -- Web dev
+                                "html", "css", "javascript",
                                 "json", "yaml", "markdown",       -- Configs & Docs
                                 "regex", "vim", "vimdoc", "query" -- Core Neovim
             },
