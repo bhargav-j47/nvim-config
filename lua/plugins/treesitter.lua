7469c662d1983bb46f97842371448a08c8c9400f
bhargav-j47 <bhargavjoshi2005@gmail.com>
2026-05-11 23:03:59 +0530

    config: treesitter: added support for new formats
    added treesitter support for
    - go
    - rust
    - html,css
    - react
    - json,yaml
    - vim core
    
    Signed-off-by: bhargav-j47 <bhargavjoshi2005@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,13 @@ return {
     config = function()
         local configs = require("nvim-treesitter")
         configs.setup({
-            ensure_installed = {"lua", "c", "javascript" , "python" ,"cpp"},
+            ensure_installed = {"lua", "c", "javascript" , "python" ,"cpp",
+                                "go", "gomod", "gowork", "gosum", -- Go ecosystem
+                                "rust", "bash", "typescript", "tsx",
+                                "html", "css",                    -- Web dev
+                                "json", "yaml", "markdown",       -- Configs & Docs
+                                "regex", "vim", "vimdoc", "query" -- Core Neovim
+            },
             highlight = { enable = true },
             indent = { enable = true }
         })
