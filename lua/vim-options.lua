164dd698ba7461c007e6ca7e7733d57353d9f52c
Bhargav Joshi <j.bhargav.u@gmail.com>
2026-06-29 00:03:38 +0530

    config: cust-commands: Add new command for removing trailing spaces
    Add new command Rmtrailspaces which removes all trailing white spaces or
    tabs from the current file.
    
    Signed-off-by: Bhargav Joshi <j.bhargav.u@gmail.com>

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -19,6 +19,7 @@ vim.opt.smarttab = true
 -- Syntax highlighting is a command, not a standard option
 vim.cmd.syntax("enable")
 
+-- general linux 8 space hard tabs
 vim.api.nvim_create_user_command('Linuxtabs', function()
     vim.opt.tabstop = 8
     vim.opt.softtabstop = 8
@@ -26,3 +27,11 @@ vim.api.nvim_create_user_command('Linuxtabs', function()
     vim.opt.expandtab = false
     print("8-space hard tabs enabled.")
 end, { desc = 'Temporarily set 8-space hard tabs' })
+
+-- Automatically remove trailing whitespace on save
+vim.api.nvim_create_user_command('Rmtrailspaces', function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+    print("Trailing spaces removed.")
+end, { desc = 'Removes trailing spaces silently' })
