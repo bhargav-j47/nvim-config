289f9ea74ad333c6d156b5c8a282750528f8a032
Bhargav Joshi <j.bhargav.u@gmail.com>
2026-07-07 16:53:26 +0530

    plugins: harpoon: It now supports shortcut uptil 7 files
    Signed-off-by: Bhargav Joshi <j.bhargav.u@gmail.com>

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,6 +13,9 @@ return {
     vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "harpoon: Go to file [2]" })
     vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "harpoon: Go to file [3]" })
     vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "harpoon: Go to file [4]" })
+    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "harpoon: Go to file [5]" })
+    vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "harpoon: Go to file [6]" })
+    vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end, { desc = "harpoon: Go to file [7]" })
     vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end, { desc = "harpoon: [P]revious file" })
     vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end, { desc = "harpoon: [N]ext file" })
 
